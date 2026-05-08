0faa8313a0b0c0084e78fc55550f16329336ec4e
chrismclarke <chris.m.clarke@live.co.uk>
2026-05-08 00:08:12 -0700

    chore: tidying

--- TODO.md
+++ TODO.md
@@ -2,6 +2,11 @@
 - [ ] Wipe server cache
 - [ ] Consider dropping osmix in favour of native osmium (run via docker container)
       Placeholder methods created
+- [ ] Update readme
+- [ ] Fix tests
+
+- [ ] Check whether mapshaper still required now using geofabrik
+- [ ] Improve frontend communication (webhook messages/webrtc)
 - [ ] Consider dropping Overpass integration entirely
 - [ ] Mount GCS volume and refactor to just use local cache system
 - [ ] Ensure map tile export works on prod (currently broken due to mkdir)
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -200,7 +200,7 @@ function App() {
           <div className="absolute inset-0 bg-white/50 backdrop-blur-sm z-[400] flex items-center justify-center">
             <div className="bg-white px-6 py-3 rounded-full shadow-lg border border-slate-200 font-medium text-slate-700 flex items-center gap-3">
               <div className="w-4 h-4 border-2 border-slate-600 border-t-transparent rounded-full animate-spin"></div>
-              Processing Overpass Data...
+              Processing Data...
             </div>
           </div>
         )}
